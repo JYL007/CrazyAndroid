44142a765549832b993d0d6706bcc6fa4caaf785
JYL007 <yongli1009@qq.com>
2018-05-17 15:50:11 +0800

    添加自定义View组件

--- app/src/main/java/com/study/yongl/crazyandroid/MainActivity.java
+++ app/src/main/java/com/study/yongl/crazyandroid/MainActivity.java
@@ -2,6 +2,11 @@ package com.study.yongl.crazyandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout layout=new LinearLayout(this);
+        super.setContentView(layout);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final TextView show=new TextView(this);
+        Button bn=new Button(this);
+        bn.setText(R.string.OK);
+        bn.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        layout.addView(bn);
+        layout.addView(show);
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show.setText(R.string.Hello+",Android,"+new java.util.Date());
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/study/yongl/crazyandroid/ViewActivity.java
@@ -0,0 +1,12 @@
+package com.study.yongl.crazyandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
